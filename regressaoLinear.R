202c6823e962eff369efa83d271064a38e4fec67
Rodrigo Tenório de Boa Ventura <boaventura19@yahoo.com.br>
2020-02-22 20:53:25 -0300

    Update regressaoLinear.R
    atualizacao.

--- regressaoLinear.R
+++ regressaoLinear.R
@@ -6,9 +6,7 @@ library(readxl)
 library(caret)
 library(e1071)
 
-
-
-custo_tratamento <- read_excel("Downloads/custo_tratamento.xlsx")
+custo_tratamento <- read_excel("C:/Users/RTenorio/Downloads/custo_tratamento.xls")
 View(custo_tratamento)
 
 attach(custo_tratamento)
@@ -16,8 +14,8 @@ dim(custo_tratamento)
 
 dim(custo_tratamento)
 
-#Qual é a variável que possui a maior correlação com a variável custo? É uma correlação positiva ou negativa? 
-  #qual é a interpretação dessa correlação?
+#Qual é a variável que possui a maior correlação com a variável custo? É uma correlação positiva ou negativa?
+#qual é a interpretação dessa correlação?
 names(custo_tratamento)
 correlacao_tr<-custo_tratamento[,c("id","gravidade_doenca","idade","tempo_internacao","custo")]
 head(correlacao_tr)
@@ -34,14 +32,50 @@ teste<-custo_tratamento[-treinamento,]
 dim(teste)
 
 
-#Faça uma análise dos resíduos utilizando os gráficos (boxplot, histograma, qqnorm, plot e tira a média dos resíduos), 
+#Regressão Linear Simples
+simples_tr<-lm(tr_modelo$custo ~tr_modelo$tempo_internacao)
+anova(simples_tr)
+summary(simples_tr)
+
+residuos_tr<-residuals(simples_tr)
+ajuste_tr<-fitted(simples_tr)
+
+tr_modelo$residuos_tr<-residuos_tr
+tr_modelo$ajuste_tr<-ajuste_tr
+
+head(tr_modelo)
+
+
+
+
+
+#Faça uma análise dos resíduos utilizando os gráficos (boxplot, histograma, qqnorm, plot e tira a média dos resíduos),
 #qual sua conclusão? O modelo está bom para prever novos dados ou não? Justifique sua resposta.
-kurtosis(custo_tratamento$custo)
-skewness(custo_tratamento$custo)
-hist(custo_tratamento$custo)
-boxplot(custo_tratamento$custo, outline = F)
-qqnorm(custo_tratamento$custo)
-qqline(custo_tratamento$custo, col="blue")
+
+boxplot(custo_tratamento$custo)
+boxplot(custo_tratamento$gravidade_doenca)
+boxplot(custo_tratamento$idade)
+boxplot(custo_tratamento$tempo_internacao)
+
+simples<- lm(custo_tratamento$custo~custo_tratamento$tempo_internacao)
+anova(simples)
+summary(simples)
 
 
+residuos<-residuals(simples)
+ajuste<-fitted(simples)
+custo_tratamento$ajuste<-ajuste
+custo_tratamento$residuos<-residuos
+
+head(custo_tratamento)
+mean(residuos)
+median(residuos)
+boxplot(residuos)
+plot(residuos)
+hist(residuos)
+qqnorm(residuos)
+qqline(residuos)
+
+
+#previsao
 
