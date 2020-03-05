8d4c43920efba68c9f43cb6a232e621a07a188fd
Rodrigo Tenório de Boa Ventura <boaventura19@yahoo.com.br>
2020-03-05 17:59:57 -0300

    Update Machine Learning 1: Análises Preditivas.R
    atualizacao.

--- Machine Learning 1: Análises Preditivas.R
+++ Machine Learning 1: Análises Preditivas.R
@@ -6,7 +6,7 @@ library(readxl)
 library(caret)
 library(e1071)
 
-custo_tratamento <- read_excel("Downloads/custo_tratamento.xlsx")
+custo_tratamento <- read_excel("C:/Users/RTenorio/Downloads/custo_tratamento.xlsx")
 View(custo_tratamento)
 
 attach(custo_tratamento)
@@ -17,14 +17,14 @@ dim(custo_tratamento)
 #Qual é a variável que possui a maior correlação com a variável custo? É uma correlação positiva ou negativa?
 #qual é a interpretação dessa correlação?
 names(custo_tratamento)
-correlacao_tr<-custo_tratamento[,c("id","gravidade_doenca","idade","tempo_internacao","custo")]
+correlacao_tr<-custo_tratamento[,c("gravidade_doenca","idade","tempo_internacao","custo")]
 head(correlacao_tr)
 
 
 rcorr(as.matrix(correlacao_tr))
 
 #Separa o arquivo em amostra de treinamento e teste.
-treinamento <-createDataPartition(custo_tratamento$id, p=.7, list = FALSE, times = 1)
+treinamento <-createDataPartition(custo_tratamento$custo, p=.7, list = FALSE, times = 1)
 tr_modelo <-custo_tratamento[treinamento,]
 dim(tr_modelo)
 
